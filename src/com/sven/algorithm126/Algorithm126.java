47bc5063d55288a0518875490ad0e054ab496baf
sven <chenli213311@sohu-inc>
2018-06-04 09:12:41 +0800

    add

--- src/com/sven/algorithm126/Algorithm126.java
+++ src/com/sven/algorithm126/Algorithm126.java
@@ -13,39 +13,62 @@ public class Algorithm126 {
 }
 
 class Solution {
+
+    private List<List<String>> ans = new ArrayList<>();
+
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
 
-        int n = beginWord.length();
-        Queue<String> que = new LinkedList<>();
-        que.add(beginWord);
-        int length = 1;
-        List<List<String>> ans = new ArrayList<>();
         Set<String> wordSet = new HashSet<>(wordList);
         if (!wordSet.contains(endWord)) return ans;
-        wordSet.remove(beginWord);
+
+        int n = beginWord.length(), size = wordSet.size();
+        Map<String, Integer> vis = new HashMap<>();
+        Map<String, List<String>> next = new HashMap<>();
+        Queue<String> que = new LinkedList<>();
+        List<String> path = new ArrayList<>();
+        ans.clear();
+        que.add(beginWord);
+        vis.put(beginWord, 0);
 
         while (!que.isEmpty()) {
-            int size = que.size();
-            for (int cnt = 0; cnt < size; cnt++) {
-                String word = que.poll();
-                char[] chs = word.toCharArray();
-                for (int j = 0; j < n; j++) {
-                    char oldCh = chs[j];
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        if (c == oldCh) continue;
-                        chs[j] = c;
-                        String tmp = new String(chs);
-                        if (tmp.equals(endWord)) return length + 1;
-                        if (wordSet.contains(tmp)) {
-                            que.add(tmp);
-                            wordSet.remove(tmp);
-                        }
+            String s = que.poll();
+            if (s.equals(endWord)) break;
+            int step = vis.get(s);
+            List<String> sNext = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                char[] chs = s.toCharArray();
+                for (char c = 'a'; c <= 'z'; c++) {
+                    chs[i] = c;
+                    String tmp = new String(chs);
+                    if (c == s.charAt(i) || !wordSet.contains(tmp)) continue;
+                    if (!vis.containsKey(tmp)) {
+                        que.add(tmp);
+                        vis.put(tmp, step + 1);
                     }
-                    chs[j] = oldCh;
+                    sNext.add(tmp);
                 }
             }
-            length++;
+            next.put(s, sNext);
         }
-        return 0;
+        path.add(beginWord);
+        dfsPath(path, next, vis, beginWord, endWord);
+        return ans;
+    }
+
+    private void dfsPath(List<String> path, Map<String, List<String>> next, Map<String, Integer> vis, String now, String end) {
+        if (now.equals(end)) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        if (!next.containsKey(now)) return;
+        List<String> list = next.get(now);
+        int visn = vis.get(now);
+        for (int i = 0; i < list.size(); i++) {
+            if (vis.get(list.get(i)) != visn + 1) continue;
+            path.add(list.get(i));
+            dfsPath(path, next, vis, list.get(i), end);
+            path.remove(path.size() - 1);
+        }
     }
 }
