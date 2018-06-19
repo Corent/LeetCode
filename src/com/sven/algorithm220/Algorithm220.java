f7234caff31828336ab8d59614199539b2715df0
sven <chenli213311@sohu-inc>
2018-06-19 09:29:36 +0800

    add

---
+++ src/com/sven/algorithm220/Algorithm220.java
@@ -0,0 +1,17 @@
+package com.sven.algorithm220;
+
+/**
+ * Copyright (C) 1998 - 2018 SOHU Inc., All Rights Reserved.
+ * <p>
+ *
+ * @author: sven
+ * @date: 2018/6/19
+ */
+public class Algorithm220 {
+}
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        return true;
+    }
+}
