b5f5f097b30287d645e3912b47c8f35e1a7cabc8
Sven <corelement@163.com>
2018-06-24 23:35:43 +0800

    add

--- src/com/sven/algorithm239/Algorithm239.java
+++ src/com/sven/algorithm239/Algorithm239.java
@@ -1,10 +1,30 @@
 package com.sven.algorithm239;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class Algorithm239 {
 }
 
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+
+        if (nums == null || nums.length < k || k <= 0) return new int[0];
+
+        List<Integer> list = new ArrayList<>(nums.length - k + 1);
+        Deque<Integer> que = new ArrayDeque<>(k);
+        for (int i = 0, n = nums.length; i < n; i++) {
+            if (!que.isEmpty() && (i - que.peekFirst() == k)) que.pollFirst();
+            while (!que.isEmpty() && nums[i] >= nums[que.peekLast()]) que.pollLast();
+            que.add(i);
+            if (i >= (k - 1)) list.add(nums[que.peekFirst()]);
+        }
+
+        int m = list.size();
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) ans[i] = list.get(i);
+        return ans;
     }
 }
---
+++ src/com/sven/algorithm240/Algorithm240.java
@@ -0,0 +1,10 @@
+package com.sven.algorithm240;
+
+public class Algorithm240 {
+}
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return false;
+    }
+}
