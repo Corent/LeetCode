6f34efcdd0cd755411604808a97edcca395b9d4b
chlim <chlim@amazon.com>
2018-06-29 00:04:05 +0800

    add

--- src/com/sven/algorithm240/Algorithm240.java
+++ src/com/sven/algorithm240/Algorithm240.java
@@ -5,6 +5,13 @@ public class Algorithm240 {
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if (target < matrix[i][j]) j--;
+            else i++;
+        }
         return false;
     }
 }
---
+++ src/com/sven/algorithm241/Algorithm241.java
@@ -0,0 +1,14 @@
+package com.sven.algorithm241;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm241 {
+}
+
+class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> ans = new ArrayList<>();
+        return ans;
+    }
+}
