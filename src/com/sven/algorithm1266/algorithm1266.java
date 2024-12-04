d4594342f17745c22411336c71f138c200721888
lichen.sven <lichen.sven@bytedance.com>
2024-12-05 00:50:00 +0800

    update

--- src/com/sven/algorithm1266/algorithm1266.java
+++ src/com/sven/algorithm1266/algorithm1266.java
@@ -5,6 +5,13 @@ public class algorithm1266 {
 
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
-        return 0;
+        int ans = 0, x0 = points[0][0], x1 = points[0][1];
+        for (int i = 1; i < points.length; ++i) {
+            int y0 = points[i][0], y1 = points[i][1];
+            ans += Math.max(Math.abs(x0 - y0), Math.abs(x1 - y1));
+            x0 = y0;
+            x1 = y1;
+        }
+        return ans;
     }
 }
---
+++ src/com/sven/algorithm1282/algorithm1282.java
@@ -0,0 +1,12 @@
+package com.sven.algorithm1282;
+
+import java.util.List;
+
+public class algorithm1282 {
+}
+
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        return null;
+    }
+}
