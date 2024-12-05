04fcaf59ec7d7592fe90005dcc589c816817a8c7
lichen.sven <lichen.sven@bytedance.com>
2024-12-05 14:36:29 +0800

    update

---
+++ src/com/sven/algorithm1238/algorithm1238.java
@@ -0,0 +1,12 @@
+package com.sven.algorithm1238;
+
+import java.util.List;
+
+public class algorithm1238 {
+}
+
+class Solution {
+    public List<Integer> circularPermutation(int n, int start) {
+        return null;
+    }
+}
--- src/com/sven/algorithm1282/algorithm1282.java
+++ src/com/sven/algorithm1282/algorithm1282.java
@@ -1,12 +1,36 @@
 package com.sven.algorithm1282;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class algorithm1282 {
 }
 
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        return null;
+        Map<Integer, List<Integer>> groups = new HashMap<Integer, List<Integer>>();
+        int n = groupSizes.length;
+        for (int i = 0; i < n; i++) {
+            int size = groupSizes[i];
+            groups.putIfAbsent(size, new ArrayList<Integer>());
+            groups.get(size).add(i);
+        }
+        List<List<Integer>> groupList = new ArrayList<List<Integer>>();
+        for (Map.Entry<Integer, List<Integer>> entry : groups.entrySet()) {
+            int size = entry.getKey();
+            List<Integer> people = entry.getValue();
+            int groupCount = people.size() / size;
+            for (int i = 0; i < groupCount; i++) {
+                List<Integer> group = new ArrayList<Integer>();
+                int start = i * size;
+                for (int j = 0; j < size; j++) {
+                    group.add(people.get(start + j));
+                }
+                groupList.add(group);
+            }
+        }
+        return groupList;
     }
 }
